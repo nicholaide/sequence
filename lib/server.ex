a06076761bd9465472f3fac4bbcad1623d396af7
Nicholai de Guzman <Nicholai@cchinfatt.local>
2016-04-23 17:59:21 -0500

    added API

--- lib/server.ex
+++ lib/server.ex
@@ -1,6 +1,24 @@
 defmodule Sequence.Server do
   use GenServer # this handles all the callbacks
   
+  ####
+  # External API
+  
+  def start_link(current_number) do
+    GenServer.start_link(__MODULE__, current_number, name: __MODULE__)
+  end
+  
+  def next_number do
+    GenServer.call __MODULE__, :next_number    
+  end
+  
+  def increment_number(delta) do
+    GenServer.cast __MODULE__, {:increment_number, delta}
+  end
+  
+  ####
+  # GenServer implementation
+  
   def handle_call(:next_number, _from, current_number) do
     { :reply, current_number, current_number + 1 }
   end
@@ -16,7 +34,5 @@ defmodule Sequence.Server do
   def handle_cast({:increment_number, delta}, current_number) do
     { :noreply, current_number + delta }
   end
-  
-  
   
 end
